1d4ee6889b84e8dda4b4fcb932973cc42073f7fc
Dominik Sliwka <dominik.sliwka@post.cz>
2022-11-11 13:22:51 +0100

    Fix typo

--- WordCipherApp/src/cz/osu/word/cipher/app/exceptions/AnyTextToEncryptException.java
+++ WordCipherApp/src/cz/osu/word/cipher/app/exceptions/AnyTextToEncryptException.java
@@ -2,6 +2,6 @@ package cz.osu.word.cipher.app.exceptions;
 
 public class AnyTextToEncryptException extends Exception {
     public AnyTextToEncryptException() {
-        super("\t-není zadaný žáden text, který by moh být šifrován");
+        super("\t-není zadaný žáden text, který by mohl být šifrován");
     }
 }
